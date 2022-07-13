81074283bb3d9d17d448c4b4522427f2870be78b
Sergey <Flargoin@gmail.com>
2022-07-13 18:21:22 +0300

    first commit 3project

---
+++ Pulse(3project)/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        prevArrow: `<button type="button" class="slick-prev"><img src='img/icons/left.svg' alt='arrow-left'></button>`,
+        nextArrow: `<button type="button" class="slick-next"><img src='img/icons/right.svg' alt='arrow-right'></button>`,
+        speed: 1200,
+    });
+  });
